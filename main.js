7c34b1d3c4501834dd04da76280ed5ec43b43ee1
pulidoandtiamson <112919025+pulidoandtiamson@users.noreply.github.com>
2022-09-06 11:28:17 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () =>{
+
+    const loginForm=document.querySelector("#login");
+    const CreateAccountForm=document.querySelector("#createAccount");
+
+    document.querySelector("#link_create_account").addEventListener("click",e =>{
+        e.preventDefault();
+        loginForm.classList.add("form--hidden");
+        CreateAccountForm.classList.remove("form--hidden");
+    }) ;
+    document.querySelector("#linkLogin").addEventListener("click", e=>{
+        e.preventDefault();
+        loginForm.classList.remove("form--hidden");
+        CreateAccountForm.classList.add("form--hidden");
+
+    });
+
+
+});
